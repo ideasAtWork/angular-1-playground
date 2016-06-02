80a0fff0b9bfafe232d12424280684a3523af7dc
Cassian Lup <cassi.lup@gmail.com>
2016-06-02 12:50:41 +0300

    cleaning up 06-routing

--- 06-routing/app/app.js
+++ 06-routing/app/app.js
@@ -7,22 +7,34 @@ angular.module('awesomeApp', ['ui.router'])
   // Now set up the states
   $stateProvider
   .state('state1', {
-    url: "/page1/:firstParam",
+    url: "/page1",
     templateUrl: "app/partials/page1.html"
   })
-  .state('state1.list', {
-    url: "/lista-mea",
-    templateUrl: "app/partials/page1.list.html"
+  .state('state2', {
+    url: "/page2",
+    templateUrl: "app/partials/page2.html"
   })
-  // .state('page2', {
-  //   url: "/page2",
-  //   templateUrl: "app/partials/page2.html"
-  // })
-  // .state('page2.list', {
-  //   url: "/list",
-  //   templateUrl: "app/partials/page2.list.html",
-  //   controller: function($scope) {
-  //     $scope.things = ["A", "Set", "Of", "Things"];
-  //   }
-  // })
 });
+
+
+
+
+
+
+
+
+
+// PARAMS
+
+// .state('state1', {
+//   url: "/page1/:firstParam",
+//   templateUrl: "app/partials/page1.html"
+// })
+
+
+// NESTED
+
+// .state('state1.list', {
+//   url: "/lista-mea",
+//   templateUrl: "app/partials/page1.list.html"
+// })
--- 06-routing/app/first.controller.js
+++ 06-routing/app/first.controller.js
@@ -1,20 +1,44 @@
 angular.module('awesomeApp')
 .controller('firstController', function($rootScope, $scope, $log, nameService, $stateParams, $state) {
-  $log.debug('my parameters are:', $stateParams);
-
-  $scope.myPassedParameters = $stateParams;
-
   $scope.names = nameService.getNames();
 
   $scope.getName = function() {
     nameService.getNewName();
   };
-
-  $scope.goToList = function() {
-    $state.go('state1.list');
-  };
-
-  $scope.isParentState = function() {
-    return $state.current.name == "state1";
-  }
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// StateParams
+
+ // $log.debug('my parameters are:', $stateParams);
+ //
+ // $scope.myPassedParameters = $stateParams;
+ //
+
+
+
+// State GoTo
+
+ // $scope.goToList = function() {
+ //   $state.go('state1.list');
+ // };
+
+
+
+// State Current
+
+ // $scope.isParentState = function() {
+ //   return $state.current.name == "state1";
+ // }
