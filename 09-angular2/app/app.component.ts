c7acef6b1aabcc2fa8a2af0171cc2ed6ee08a77a
Cassian Lup <cassi.lup@gmail.com>
2016-06-15 12:01:10 +0300

    09-angular2

---
+++ 09-angular2/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: "<h1>Advanced UI, Angular 2 Example</h1>"
+})
+export class AppComponent {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'my-app',
+//   template: `
+//     <h1>My First Angular 2 App</h1>
+//     <input type="text" [(ngModel)]="myField"> {{myField}}
+//     <input type="text" [value]="myField">
+//     <input type="text" (click)="sayHi()">
+//   `
+// })
+// export class AppComponent {
+//   myField = "";
+//
+//   sayHi = () => window.alert('hi');
+// }
